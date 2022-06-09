7af688a348cbf60ebff09f8ff1c73b09509f46f2
Orbp1403 <2492944860101@ingenieria.usac.edu.gt>
2022-06-09 09:35:32 -0600

    Arreglos para levantar las aplicaciones en contenedores

--- backend/configdb.js
+++ backend/configdb.js
@@ -1,7 +1,7 @@
 const conn = new (require("rest-mssql-nodejs"))({
   user: "pruebasuvg",
   password: "pruebasuvg",
-  server: "127.0.0.1",
+  server: "192.168.1.9",
   database: "PRUEBASUVG",
   port: 19791,
   options: {
